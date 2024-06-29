155c7908157f26e25963884ca02f50a1f639056d
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-29 10:46:55 +0300

    added services and packages

---
+++ Music.Core/Services/IArtistService.cs
@@ -0,0 +1,18 @@
+﻿using Music.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music.Core.Services
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<Artist>> GetAllArtists();
+        Task<Artist> GetArtistById(int id);
+        Task<Artist> CreateArtist(Artist newArtist);
+        Task UpdateArtist(Artist artistToBeUpdated, Artist artist);
+        Task DeleteArtist(Artist artist);
+    }
+}
---
+++ Music.Core/Services/ITrackService.cs
@@ -0,0 +1,19 @@
+﻿using Music.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music.Core.Services
+{
+    public interface ITrackService
+    {
+        Task<IEnumerable<Track>> GetAllWithArtistAsync();
+        Task<Track> GetTrackByIdAsync(int id);
+        Task<IEnumerable<Track>> GetTracksByArtistIdAsync(int artistId);
+        Task<Track> CreateTrackAsync(Track newTrack);
+        Task UpdateTrackAsync(Track trackToBeUpdated, Track track);
+        Task DeleteTrackAsync(Track track);
+    }
+}
